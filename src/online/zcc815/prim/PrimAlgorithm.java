14e8f9add08d1a822378889406db52cf96e50690
zcc815 <zcc815@126.com>
2020-11-24 18:01:46 +0800

    prim算法解决修路问题(最消耗)
    Signed-off-by: zcc815 <zcc815@126.com>

--- src/online/zcc815/prim/PrimAlgorithm.java
+++ src/online/zcc815/prim/PrimAlgorithm.java
@@ -28,8 +28,9 @@ public class PrimAlgorithm {
 
         MGraph graph = new MGraph(verxs);
         MinTree minTree = new MinTree();
-        minTree.createGraph(graph,verxs,data,weight);
+        minTree.createGraph(graph, verxs, data, weight);
         minTree.showGraph(graph);
+        minTree.prim(graph,1);
 
     }
 }
@@ -50,6 +51,35 @@ class MinTree {
         for (int[] link : graph.weight) {
             System.out.println(Arrays.toString(link));
         }
+    }
+
+    public void prim(MGraph graph, int v) {
+        //创建数组用来标记该节点是否被访问过
+        int[] visited = new int[graph.verxs];
+        //表明当前节点已经被访问过
+        visited[v] = 1;
+        //记录两个顶点的下标
+        int h1 = -1;
+        int h2 = -1;
+        int minWeight = 10000;
+        for (int i = 1; i < graph.verxs; i++) { //n个顶点会产生n-1条边
+            for (int j = 0; j < graph.verxs; j++) {//j节点访问过的
+                for (int k = 0; k < graph.verxs; k++) {//k节点访问过的
+                    if (visited[j] == 1 && visited[k] == 0 && graph.weight[j][k] < minWeight) {
+                        //寻找已经访问过的节点何为访问过的节点间的权值最小的边
+                        minWeight = graph.weight[j][k];
+                        h1 = j;
+                        h2 = k;
+                    }
+                }
+            }
+            System.out.println("边<" + graph.data[h1]+","+graph.data[h2]+">权值"+minWeight);
+            //将当前节点标记为已经访问过
+            visited[h2] =1;
+            minWeight = 10000;
+        }
+
+
     }
 }
 
